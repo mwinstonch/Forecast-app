d7464452874e0401af16a46f2605344385b7a5ea
Michael Churchill <mwinstonch@gmail.com>
2016-07-13 12:20:35 -0500

    no message

--- main.js
+++ main.js
@@ -21,6 +21,16 @@ function getQuery(keyEvent) {
     }
 }
 
+var weekObject = {
+        0: "Sunday",
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday"
+}
+
 var showWeather = function(jsonData) {
     console.log(jsonData)
     var day = new Date()
