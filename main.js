255d5687e9699cf56b3728df98b0cd51cd889397
Michael Churchill <mwinstonch@gmail.com>
2016-07-21 09:48:19 -0500

    no message

--- main.js
+++ main.js
@@ -49,7 +49,10 @@ var showWeather = function(jsonData) {
             marginBottom: 0,
             marginTop: 0
         }
-        weatherView.innerHTML = ''
+        weatherView.innerHTML = '<div class=""><h1 class="currentTemp">' + parseInt(jsonData.currently.temperature) + '&deg;F</h1>\
+            <img class="weatherIcon" src="images/' + jsonData.currently.icon + '.png"></div>\
+            <div class="daySummary"><h3 class="weekday">' + week[today] + '</h3>' +
+            '<h3 class="summary">' + jsonData.currently.summary + '</h3></div>'
         forecastDiv.innerHTML = ''
         forecastDiv.innerHTML += '<div class="currentForecast">' +
             '<p>Feels like: ' + parseInt(jsonData.currently.apparentTemperature) + '&deg;F</p>' +
@@ -84,6 +87,7 @@ var showWeather = function(jsonData) {
                <h2 class="minTemp">' + day.temperatureMin + '°</h2>\
                    </div>'
         }
+    weatherView.innerHTML = ''
 	forecastDiv.innerHTML = htmlString
     }
 
